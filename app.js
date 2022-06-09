69bebcb9d78503226b56d63221678f75460ec14a
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-09 16:57:44 +0200

    continued styling navbar, now working on js file

---
+++ app.js
@@ -0,0 +1,13 @@
+//mobile targets the menu flip
+const menu = document.querySelector('#mobile-menu')
+//mobile targets the menu links, class .navbar__menu
+const menuLinks = document.querySelector('.navbar__menu')
+
+//toggling the menu - display mobile menue
+//we want to trigger navbar__menu.active and the animation effects of nth child 1-3
+const mobileMenu = () => {
+    menu.classList.toggle('is-active') //basicall active, or not active
+    menuLinks.classList.toggle('active')
+}
+
+menu
